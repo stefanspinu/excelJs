9f7355531e200515f4e16a35ce1fbfc44f1ce158
Stefan <stefanspinu3@gmail.com>
2020-12-14 11:22:10 +0200

    Finish base table logic

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -4,12 +4,14 @@ const CODES = {
   Z: 90
 }
 
-function toCell(_, col) {
-  return `
-  <div class="cell" contenteditable="" data-col="${col}">
+function toCell(row) {
+  return function(_, col) {
+    return `
+    <div class="cell" contenteditable="" data-col="${col}" data-id="${row}:${col}" data-type="cell">
 
-  </div>
-  `
+    </div>
+    `
+  }
 }
 
 function toColumn(col, index) {
@@ -50,12 +52,12 @@ export function createTable(rowsCount = 15) {
 
   rows.push(createRow(null, cols))
 
-  for (let i = 0; i <= rowsCount; i++) {
+  for (let row = 0; row <= rowsCount; row++) {
     const cells = new Array(colsCount)
       .fill('')
-      .map(toCell)
+      .map(toCell(row))
       .join('')
-    rows.push(createRow(i + 1, cells))
+    rows.push(createRow(row + 1, cells))
   }
   
   return rows.join('')

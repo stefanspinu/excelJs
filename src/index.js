96ee0e57866ff75020a313c14c5ffeb2bff0214f
Stefan <stefanspinu3@gmail.com>
2020-12-15 16:02:41 +0200

    Finished redux and table logic 2

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ import './scss/index.scss'
 const store = createStore(rootReducer, initialState) 
 
 const stateListener = debounce(state => {
-  console.log('App State: ', state)
   storage('excel-state', state)
 }, 300)
 
